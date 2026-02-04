5b0250d37fc9c6f6374cd511c405c71ba27e48ea
Kaleab85 <kaleabberhanu1@outlook.com>
2026-02-04 16:55:38 +0300

    refact: reset entire project

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga veniam non consequuntur unde nemo, aliquam quos, velit a earum hic, id ad! Vero quisquam repellat, laborum eius corrupti consequatur itaque.</p>
+    </div>
+  )
+}
+
+export default HomePage
